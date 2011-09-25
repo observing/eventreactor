4defac46a0825108a648933db66fe3253fecb2b5
Arnout Kazemier <info@3rd-Eden.com>
2011-09-25 11:39:55 +0200

    inital working version

---
+++ tests/eventreactor.test.js
@@ -0,0 +1,67 @@
+var EventEmitter = process.EventEmitter
+  , should = require('should');
+
+  // initialize our module
+  require('../lib');
+
+  module.exports = {
+      'has event': function () {
+        var EE = new EventEmitter
+          , example = function () {};
+
+        EE.on('example', example);
+
+        // test this shizz
+        EE.has('example', example).should.be_true;
+        EE.has('testcase', example).should.be_false;
+        EE.has('example', function () {}).should.be_false;
+      }
+
+    , 'subscribe to every given event': function () {
+        var EE = new EventEmitter
+          , count = 0;
+
+        EE.every('events', 'here', 'could', 'be', 'fired', function () {
+          ++count;
+        });
+
+        EE.emit('events');
+        EE.emit('here');
+        EE.emit('could');
+        EE.emit('be');
+        EE.emit('fired');
+        EE.emit('non existant');
+
+        // for some odd reason it seems like that the events are fired
+        // async, without a setTimeout it wines about 4 count's instead of 5
+        setTimeout(function () { 
+          count.should.equal(5);
+        }, 10);
+      }
+
+    , 'capture non listening events': function () {
+        var EE = new EventEmitter
+          , count = 0
+          , unexisting = [
+              'i dont exist'
+            , 'me neither'
+            , 'nor do i'
+            ]
+          , i = unexisting.length;
+
+        EE.on('uncaught', function (event, data) {
+          unexisting.indexOf(event).should.be.above(-1);
+          count++;
+        });
+
+        EE.on('exists', function () {});
+
+        while (i--) EE.emit(unexisting[i]);
+        EE.emit('exists');
+
+        // see above for the odd timeout thingy
+        setTimeout(function () {
+          count.should.equal(3);
+        }, 10);
+      }
+  };
